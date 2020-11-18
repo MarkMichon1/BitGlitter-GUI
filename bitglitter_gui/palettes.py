28d3e4554c36c62c7ec2c2d8c879ad2bb8c2d5b0
Mark Michon <markmichon7@gmail.com>
2020-11-18 16:45:03 -0600

    11/18/20 update
    - readme updated
    - all old code removed as we're starting over with a different tech stack :)

--- bitglitter_gui/palettes.py
+++
@@ -1,6 +0,0 @@
-from bitglitter.utilities.gui_utilities import return_all_palette_objects
-
-# figuring out layout of it...
-
-test = return_all_palette_objects()
-print(test)
--- bitglitter_gui/utilities.py
+++
@@ -1,3 +0,0 @@
-import base64
-
-#todo- create feature in color menu to import/export base64 encoded string containing palette data
